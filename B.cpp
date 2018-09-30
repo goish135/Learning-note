1d7edd71b3a1fbb4df388907dfef1f37f25ad699
goish135 <goish2468@gmail.com>
2018-10-01 00:28:40 +0800

    LCS

---
+++ B.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int tc;
+	scanf("%d",&tc);
+	while(tc--)
+	{
+		string s1,s2;
+		cin>>s1>>s2;
+		//cout<<s1<<s2;
+		int len_1 = s1.length();
+		int len_2 = s2.length();
+		//cout<<len_1<<" "<<len_2<<'\n';
+	}
+	return 0;
+} 
